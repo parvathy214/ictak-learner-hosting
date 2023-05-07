393167c82f92064a98d6ea1c2256952d628200af
Parvathi <p4parvathy214@gmail.com>
2023-05-07 12:50:51 +0530

    first commit

--- app.js
+++ app.js
@@ -12,11 +12,7 @@ app.use(bodyparser.json());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-//hosting
 
-const path = require('path');
-app.use(express.static('./dist/frontend/'))
-app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/frontend/index.html')); });
 
 const mongoose = require('./db')
 const api = require('./routes/api')
@@ -24,6 +20,12 @@ app.use('/api',api)
 
 const learner = require('./routes/learner')
 app.use('/api',learner)
+
+//hosting
+
+const path = require('path');
+app.use(express.static('./dist/frontend/'))
+app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/frontend/index.html')); });
 
 
 app.listen(2341,()=>{
